e291bd5ff27009d49d46a825537f15b4e42e9648
Sébastien Forestier <sebastien.forestier@inria.fr>
2017-05-10 16:13:09 +0200

    analysis

--- scripts/analyze_explo.py
+++ scripts/analyze_explo.py
@@ -6,21 +6,37 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import brewer2mpl
-bmap = brewer2mpl.get_map('Dark2', 'qualitative', 4)
+bmap = brewer2mpl.get_map('Dark2', 'qualitative', 5)
 colors = bmap.mpl_colors
 
-# PARAMS
-path = "/home/sforesti/scm/Flowers/NIPS2017/data/logs/"
-experiment_name = "seb_holidays"
-configs = dict(AMB=3, RMB=3)
-#configs = dict(RmB=1, AMB=5)
 
-config_colors = dict(AMB=colors[0], RmB=colors[1], FGB=colors[2], RMB=colors[3])
+
+simu = True
+
+if simu:
+    
+    # From SIMU
+    path = "/home/sforesti/catkin_ws/src/nips2017/logs/"
+    experiment_name = "experiment"
+    configs = dict(AMB=3, RMB=3, RmB=3, FC=1, OS=3)
+    n = 10000
+    j_error = 0.1
+else:
+    
+    # PARAMS
+    path = "/home/sforesti/scm/Flowers/NIPS2017/data/logs/"
+    experiment_name = "nips_4_mai"
+    configs = dict(AMB=2, RMB=3, RmB=1, FC=3, OS=3)
+    n = 5000
+    j_error = 0.02
+
+
+
+config_colors = dict(AMB=colors[0], RmB=colors[1], FC=colors[2], RMB=colors[3], OS=colors[4])
 
 
 sw = 20
 n_logs = 1
-n = 5000
 p = 100
 x = np.array(np.linspace(0,n,n/p+1), dtype=int)
 
@@ -91,8 +107,10 @@ if True:
                 jr_touch = 0
                 e_touch = 0
                 b_touch = 0
+                l_touch = 0
+                s_touch = 0
                 
-                for i in range(n):
+                for i in range(len(log["sm_data"]["mod1"][1])):
                     #print i, "joystick right", list(log["sm_data"]["mod2"][1][i])
                     #print "ergo", list(log["sm_data"]["mod4"][1][i])[1:]
                     #print "ball", list(log["sm_data"]["mod5"][1][i])[2:]
@@ -102,27 +120,36 @@ if True:
                         #print i, "Ball:", log["sm_data"]["mod5"][1][i][[2+4, 2+5, 2+8, 2+9, 2+12, 2+13, 2+16, 2+17]]
                         #print i, "Light:", log["sm_data"]["mod6"][1][i]
                     #print i
-                    if np.linalg.norm(log["sm_data"]["mod2"][1][i] - np.array([-1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1., 0., -1.,  0., -1.,  0., -1.,  0.])) > 0.05:
+                    if np.linalg.norm(log["sm_data"]["mod2"][1][i] - np.array([-1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1., 0., -1.,  0., -1.,  0., -1.,  0.])) > j_error:
                         jr_touch += 1
                         #print
-                        #print i, "joystick right", list(log["sm_data"]["mod2"][1][i])
-                    if np.linalg.norm(np.array(list(log["sm_data"]["mod4"][1][i])[1:])[::2] - np.mean(np.array(list(log["sm_data"]["mod4"][1][i])[1:])[::2])) > 0.00001 or np.linalg.norm(np.array(list(log["sm_data"]["mod4"][1][i])[1:])[1::2] - np.mean(np.array(list(log["sm_data"]["mod4"][1][i])[1:])[1::2])) > 0.00001:
+                        #print i, "joystick right", list(log["sm_data"]["mod2"][1][i]), np.linalg.norm(log["sm_data"]["mod2"][1][i] - np.array([-1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1., 0., -1.,  0., -1.,  0., -1.,  0.]))
+                    if np.linalg.norm(np.array(list(log["sm_data"]["mod4"][1][i])[1:])[::2] - np.mean(np.array(list(log["sm_data"]["mod4"][1][i])[1:])[::2])) > 0.02 or np.linalg.norm(np.array(list(log["sm_data"]["mod4"][1][i])[1:])[1::2] - np.mean(np.array(list(log["sm_data"]["mod4"][1][i])[1:])[1::2])) > 0.02:
                         e_touch += 1
-                        print
-                        print i, "joystick right", list(log["sm_data"]["mod2"][1][i])
-                        print "ergo", list(log["sm_data"]["mod4"][1][i])
+                        #print
+                        #print i, "joystick right", list(log["sm_data"]["mod2"][1][i])
+                        #print "ergo", list(log["sm_data"]["mod4"][1][i])
                             
-                    if abs(list(log["sm_data"]["mod5"][1][i])[2:][0] -  list(log["sm_data"]["mod5"][1][i])[2:][-2]) > 0.05:
+                    if abs(list(log["sm_data"]["mod5"][1][i])[2:][0] -  list(log["sm_data"]["mod5"][1][i])[2:][-2]) > 0.02:
                         b_touch += 1
-                    if np.linalg.norm(log["sm_data"]["mod3"][1][i] - np.array([0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1., 0., -1.,  0., -1.,  0., -1.])) > 0.05:
+                    if np.linalg.norm(log["sm_data"]["mod3"][1][i] - np.array([0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1., 0., -1.,  0., -1.,  0., -1.])) > j_error:
                         jl_touch += 1
-                        #print i, "joystick left", list(log["sm_data"]["mod3"][1][i])
+                        #print i, "joystick left", list(log["sm_data"]["mod3"][1][i]), np.linalg.norm(log["sm_data"]["mod3"][1][i] - np.array([0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1.,  0., -1., 0., -1.,  0., -1.,  0., -1.]))
+                    if np.linalg.norm(log["sm_data"]["mod6"][1][i][2:] - np.array([-1., -1., -1., -1., -1., -1., -1., -1., -1., -1.])) > 0.01:
+                        l_touch += 1
+                                             
+                                             
+                    if np.linalg.norm(log["sm_data"]["mod7"][1][i][2:] - np.array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])) > 0.01:
+                        s_touch += 1
+                      
                 print
-                print "Motor Babbling Iterations:", n
+                print "Iterations:", len(log["sm_data"]["mod1"][1])
                 print "Joystick Left touched:", jl_touch, "percentage:", 100. * jl_touch / n, "%"
                 print "Joystick Right touched:", jr_touch, "percentage:", 100. * jr_touch / n, "%"
                 print "Ergo touched:", e_touch, "percentage:", 100. * e_touch / n, "%"
                 print "Ball touched:", b_touch, "percentage:", int(100. * b_touch / n), "%"
+                print "Light touched:", l_touch, "percentage:", int(100. * l_touch / n), "%"
+                print "Sound touched:", s_touch, "percentage:", int(100. * s_touch / n), "%"
                 print
                 
             
@@ -162,7 +189,11 @@ if True:
                 for s_space in explo.keys():
                     #print "Analysis", s_space
                     
-                    explo[s_space][config][trial] = compute_explo([log["sm_data"][dims[s_space]][1][i][cdims[s_space]:] for i in range(n)], -1., 1., x)
+                    try:
+                        explo[s_space][config][trial] = compute_explo([log["sm_data"][dims[s_space]][1][i][cdims[s_space]:] for i in range(len(log["sm_data"][dims[s_space]][1]))], -1., 1., x)
+                    except:
+                        print i, len(log["sm_data"][dims[s_space]][1]), log["sm_data"][dims[s_space]][1][i][cdims[s_space]:]
+                        
                     #print explo[s_space][config][trial]
                  
                  
---
+++ scripts/analyze_touch.py
@@ -0,0 +1,222 @@
+import os
+import sys
+import cPickle
+import numpy as np
+import matplotlib.pyplot as plt
+#import seaborn as sns
+
+import brewer2mpl
+bmap = brewer2mpl.get_map('Dark2', 'qualitative', 8)
+#colors = bmap.mpl_colors
+colors = {
+      "motor_babbling":bmap.mpl_colors[0], 
+      "Hand": bmap.mpl_colors[1], 
+      "Joystick_L": bmap.mpl_colors[2], 
+      "Joystick_R":bmap.mpl_colors[3], 
+      "Ergo":bmap.mpl_colors[4], 
+      "Ball":bmap.mpl_colors[5], 
+      "Light":bmap.mpl_colors[6], 
+      "Sound":bmap.mpl_colors[7]
+      }
+
+# PARAMS
+path = "/home/sforesti/scm/Flowers/NIPS2017/data/logs/"
+experiment_name = "seb_holidays"
+configs = dict(RMB=3, AMB=3)
+#configs = dict(RmB=1, AMB=5)
+
+
+n_logs = 1
+n = 5000 # Iterations taken into account
+p = 10 # Number of checkpoints
+x = range(n)
+
+
+
+def discovery(data, print_data=False):
+    n = len(data)
+    result = [0.]
+    for i in range(1, n):
+        x = data[i]
+        if np.linalg.norm(np.array(x)[::2] - np.mean(np.array(x)[::2])) < 0.01 and np.linalg.norm(np.array(x)[1::2] - np.mean(np.array(x)[1::2])) < 0.01:
+            result.append(0.)
+        else:
+            if print_data:
+                print i, x
+#             min_dist = np.inf
+#             for j in range(i):
+#                 y = data[j]
+#                 min_dist = min(min_dist, np.linalg.norm(np.array(x) - np.array(y)))
+#            result.append(min_dist)    
+            result.append(1.)    
+    return result
+
+    
+s_spaces = ["Hand", "Joystick_L", "Joystick_R", "Ergo", "Ball", "Light", "Sound"]
+
+
+if True:
+
+    explo = {}
+    explo_gain = {}
+    explo_touch = {}
+    bootstrapped_s = {}
+    
+    
+    for s_space in s_spaces:
+        explo[s_space] = {}
+        explo_gain[s_space] = {}
+        bootstrapped_s[s_space] = {}
+    
+    
+
+    for config in configs.keys():
+        
+    
+        for s_space in s_spaces:
+            explo[s_space][config] = {}
+            explo_gain[s_space][config] = {}
+            bootstrapped_s[s_space][config] = {}
+    
+        explo_touch[config] = {}
+
+
+        for trial in range(configs[config]):
+            
+            explo_touch[config][trial] = {}
+            
+            print "\nLoading", config, trial
+            
+            filename = path + experiment_name + "_" + config + "_" + str(trial) + ".pickle"
+            with open(filename, 'r') as f:
+                log = cPickle.load(f)
+            f.close()
+            
+            #print log["chosen_modules"][:50]
+            
+            dims = {"motor_babbling":"motor_babbling",
+                    "Hand":"mod1",
+                    "Joystick_L":"mod3",
+                    "Joystick_R":"mod2",
+                    "Ergo":"mod4",
+                    "Ball":"mod5",
+                    "Light":"mod6",
+                    "Sound":"mod7"}
+            
+            cdims = dict(Hand=0,
+                        Joystick_L=0,
+                        Joystick_R=0,
+                        Ergo=1,
+                        Ball=2,
+                        Light=2,
+                        Sound=2)
+            
+            
+            for s_space in explo.keys():
+                print "Analysis", s_space
+                
+                explo_gain[s_space][config][trial] = discovery([log["sm_data"][dims[s_space]][1][i][cdims[s_space]:] for i in range(n)], True if s_space == 'Ergo' else False)
+                bootstrapped_s[s_space][config][trial] = next((i for i, x in enumerate(explo_gain[s_space][config][trial]) if x), n)
+                #print s_space, explo[s_space][config][trial][:20]
+                
+            bootstrapped_s["motor_babbling"] = {}
+            bootstrapped_s["motor_babbling"][config] = {}
+            bootstrapped_s["motor_babbling"][config][trial] = 0
+            
+            for s_space1 in explo.keys()+ ["motor_babbling"]:
+                explo_touch[config][trial][s_space1] = {}
+                for s_space2 in explo.keys():
+                    explo_touch[config][trial][s_space1][s_space2] = np.zeros(p)
+                    
+            for i in range(1,n-1):
+#                 if abs(log["sm_data"]["mod4"][1][i][0] - log["sm_data"]["mod4"][1][i][-2]) > 0.1 or i in [321, 322]:
+#                     print i, "\nJ1:", log["sm_data"]["mod2"][1][i]
+#                     print i, "\nJ2:", log["sm_data"]["mod3"][1][i]
+#                     print i, "\nErgo:", log["sm_data"]["mod4"][1][i]
+#                     print i, "\nBall:", log["sm_data"]["mod5"][1][i]
+                for s_space1 in explo_touch[config][trial].keys():
+                    mid = dims[s_space1]
+                    if mid == log["chosen_modules"][i]:
+                        for s_space2 in explo.keys():
+                            #print
+                            g = explo_gain[s_space2][config][trial][i]
+                            #if g > 0:# and s_space2 in ["Joystick_L", "Ergo"]:
+                                #m = log["sm_data"]["mod1"][0][i]
+                                #print "Iteration:", i, ", Chosen space:", s_space1, ", Gain in", s_space2, ":", g, "bootstrapped_s:", bootstrapped_s[s_space1][config][trial]
+#                                 print i, "\nJR:", log["sm_data"]["mod2"][1][i]
+#                                 print i, "\nJL:", log["sm_data"]["mod3"][1][i]
+#                                 print i, "\nErgo:", log["sm_data"]["mod4"][1][i]
+#                                 print i, "\nBall:", log["sm_data"]["mod5"][1][i]
+                            if g > 0:# and bootstrapped_s[s_space1][config][trial] < i:
+                                explo_touch[config][trial][s_space1][s_space2][i/(n/p)] += g
+                    
+                    
+            for s_space1 in explo_touch[config][trial].keys():
+                #print trial, s_space1, [log["chosen_modules"][i*(n/10):(i+1)*(n/10)].count(dims[s_space1]) for i in range(10)]
+                chosen = [log["chosen_modules"][j*(n/p):(j+1)*(n/p)].count(dims[s_space1]) for j in range(p)]
+                for s_space2 in explo.keys():
+                    explo_touch[config][trial][s_space1][s_space2] /= chosen
+#                     if s_space2 == "Hand":
+#                         print "   ", s_space2, explo_touch[config][trial][s_space1][s_space2]
+                    
+                    
+    with open(path + 'analysis_touch.pickle', 'wb') as f:
+        cPickle.dump(explo_touch, f)
+
+else:
+    
+    with open(path + 'analysis_touch.pickle', 'r') as f:
+        explo_touch = cPickle.load(f)
+    f.close()
+    
+    print explo_touch
+    
+    config = "AMB"
+    #trials = range(configs[config])
+    trials = [0]
+    print trials
+    
+    labels = {
+              "motor_babbling":"$Random$", 
+              "Hand": "$Hand$", 
+              "Joystick_L": "$Joystick_L$", 
+              "Joystick_R":"$Joystick_R$", 
+              "Ergo":"$Ergo$", 
+              "Ball":"$Ball$", 
+              "Light":"$Light$", 
+              "Sound":"$Sound$"
+              }
+    
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    
+    for s_space2 in ["Hand", "Joystick_L", "Joystick_R", "Ergo", "Ball"]:#, "Light", "Sound"]:
+            
+                
+        fig, ax = plt.subplots()
+        fig.canvas.set_window_title(s_space2)
+        plt.title("\% Touching $"+s_space2+"$ while exploring...", fontsize=24)
+            
+        for s_space1 in ["motor_babbling", "Hand", "Joystick_L", "Joystick_R", "Ergo", "Ball"]:#, "Light", "Sound"]:
+#             if s_space1 == "Hand" and s_space2 == "Ball":
+#                 print s_space1, s_space2, [explo_touch[config][trial][s_space1][s_space2] for trial in range(configs[config])]
+            plt.plot(np.linspace(n/p, n, p), 100. * np.mean([explo_touch[config][trial][s_space1][s_space2] for trial in trials], axis=0), lw=3, color=colors[s_space1], label=labels[s_space1])
+            
+                
+        legend = plt.legend(frameon=True, fontsize=18)
+        plt.xticks(fontsize = 16)
+        plt.yticks(fontsize = 16)
+        plt.xlabel("Iterations", fontsize=18)
+        plt.ylabel("\% Touch", fontsize=18)
+        frame = legend.get_frame()
+        frame.set_facecolor('1.')
+        frame.set_edgecolor('0.')
+        
+        #plt.savefig("/home/sforesti/scm/PhD/cogsci2016/include/obj-explo.pdf", format='pdf', dpi=100, bbox_inches='tight')
+        plt.savefig("/home/sforesti/scm/Flowers/NIPS2017/figs/touch_" + s_space2 + '.pdf', format='pdf', dpi=100, bbox_inches='tight')
+        
+        
+        plt.show(block=False)
+    plt.show()
+            
+        
