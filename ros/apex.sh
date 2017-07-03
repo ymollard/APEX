7d4f0c152c03c74baaea788bf6dc0581c7ab1b5b
Yoan Mollard <yoan.mollard@inria.fr>
2017-07-03 19:17:05 +0000

    Update manual ROS sh

---
+++ ros/apex.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+master_hostname="mingew.local"
+hostname=`hostname`
+
+env_vars="export ROS_MASTER_URI=\"http://${master_hostname}:11311\" 
+export ROS_HOSTNAME=\"${hostname}.local\" 
+export PS1=\"\[\033[00;33m\][\${ROS_MASTER_URI}]\[\033[00m\] \${PS1}\""
+
+if [ -f /home/pi/ros_ws/devel_isolated/setup.bash ]; then
+ source /home/pi/ros_ws/devel_isolated/setup.bash
+fi
+
+if [ -f /home/pi/ros_ws/devel/setup.bash ]; then
+ source /home/pi/ros_ws/devel/setup.bash
+fi
+
+bash --rcfile <(cat ~/.bashrc; echo "${env_vars}")
