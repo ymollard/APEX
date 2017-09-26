cde709fd7b62c7277058f7f7c6c76f84372d4766
Yoan Mollard <yoan.mollard@inria.fr>
2017-09-26 15:28:51 +0200

    Parallelize data fetching

--- scripts/workstation_tools/download_results.bash
+++ scripts/workstation_tools/download_results.bash
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
+# This script gathers experiment data stored on all apex-XX-torso.local robots locally.
+
 
 if [ $# != 1 ]; then
     echo -e "\e[31mUSAGE: $0 experiment_name\e[0m"
     exit 1
 fi
 
-path="/media/usb0/$1"
-experiment="$1"
+tar_and_copy() {
+    experiment=$1
+    id=$2
+    path="/media/usb0/$experiment"
 
-for id in `seq 1 6`; do
-    echo -e "Archiving data from apex-$id..."
+    echo -e "Archiving data from experiment $experiment on apex-$id..."
     ssh pi@apex-$id-torso.local "sudo tar -cf /tmp/$experiment.tar $path"
     if [ $? -eq 0 ]; then
         local_file="/tmp/$experiment-$id.tar"
@@ -17,5 +20,15 @@ for id in `seq 1 6`; do
         scp pi@apex-$id-torso.local:/tmp/$experiment.tar $local_file
         echo -e "unziping..."
         tar -xf $local_file
+        echo -e "\e[32mapex-$id DONE!\e[39"
     fi
+}
+
+export -f tar_and_copy
+
+experiment="$1"
+for id in `seq 1 6`; do
+    tar_and_copy "$experiment" "$id" &
 done
+wait
+
