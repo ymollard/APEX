2457929e7b14b32afd1047d2b5ae075b80cd67e3
Yoan Mollard <yoan.mollard@inria.fr>
2017-10-11 16:46:23 +0000

    Torso button corrections

--- ros/apex_playground/src/apex_playground/controller/perception.py
+++ ros/apex_playground/src/apex_playground/controller/perception.py
@@ -15,12 +15,12 @@ class Perception(object):
             service['call'] = rospy.ServiceProxy(service['name'], service['type'])
 
         # Buttons switches + LEDs
-        self.prefix = 'ergo'
-        self.button_leds_topic = ['button_leds/help', 'button_leds/pause']
+        self.prefix = 'sensors'
+        self.button_leds_topics = ['button_leds/help', 'button_leds/pause']
         self.buttons_topics = ['buttons/help', 'buttons/pause']
         self.subscribers = [rospy.Subscriber('/'.join([self.prefix, topic]), Bool, lambda msg: self._cb_button_pressed(msg, topic)) for topic in self.buttons_topics]
         self.publishers = {topic: rospy.Publisher('/'.join([self.prefix, topic]), Bool) for topic in self.button_leds_topics}
-        self.button_leds_state = {topic: False for topic in self.button_leds_topics}
+        self.button_leds_status = {topic: False for topic in self.button_leds_topics}
         self.button_pressed = {topic: False for topic in self.buttons_topics}
         self.last_press = {topic: rospy.Time(0) for topic in self.buttons_topics}
 
@@ -29,7 +29,6 @@ class Perception(object):
             self.params = json.load(f)
 
     def _cb_button_pressed(self, msg, topic):
-        topic = '/'.join([self.prefix, topic])
         if msg.data and rospy.Time.now() - self.last_press[topic] > rospy.Duration(self.params['duration_between_presses']):
             rospy.logwarn("Button {} pressed...".format(topic))
             self.button_pressed[topic] = True
@@ -38,18 +37,16 @@ class Perception(object):
     def has_been_pressed(self, topic):
         if topic not in self.buttons_topics:
             return False
-        topic = '/'.join([self.prefix, topic])
         pressed = self.button_pressed[topic]
         self.button_pressed[topic] = False
         return pressed
 
     def switch_led(self, topic, value=None):
-        topic = '/'.join([self.prefix, topic])
         if value is None:
             self.button_leds_status[topic] = not self.button_leds_status[topic]
         elif value in [True, False]:
             self.button_leds_status[topic] = value
-        self.publishers.publish(topic, Bool(data=self.button_leds_status[topic]))
+        self.publishers[topic].publish(Bool(data=self.button_leds_status[topic]))
 
     def record(self, human_demo, nb_points):
         call = self.services['record']['call']
